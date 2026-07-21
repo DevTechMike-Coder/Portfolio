edd71c4772f5d2979087d0503ac1a744652b8f64
DevTech <micahkesiena@gmail.com>
2026-07-21 10:11:35 +0100

    feat(ui): Added a backtotop feature with proper gsap animation to the entire project while also implement a download cv button

---
+++ src/components/icons/Download.tsx
@@ -0,0 +1,22 @@
+import type { SVGProps } from "react";
+
+export function Download(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M12 3v12" />
+      <polyline points="7 11 12 16 17 11" />
+      <path d="M5 20h14" />
+    </svg>
+  );
+}
